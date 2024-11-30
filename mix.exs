b96f6434ef6569345566c9e1538597a25924292d
vittorio-reinaudo <v.reinaudo@arkemis.it>
2024-11-30 21:00:20 +0100

    fix: mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule ExGraphQL.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/vittorio-reinaudo/ex-graphql"
+
   def project do
     [
       app: :ex_graphql,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -22,6 +25,19 @@ defmodule ExGraphQL.MixProject do
     [
       {:tesla, "~> 1.4"},
       {:jason, "~> 1.3"}
+    ]
+  end
+
+  defp package do
+    [
+      description: "GraphQL API Builder for Elixir",
+      maintainers: [
+        "Vittorio Reinaudo"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url
+      }
     ]
   end
 end
